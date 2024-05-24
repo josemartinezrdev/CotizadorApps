e3901f3b3cb8b84d399a6edfd0568869b11173b8
josemartinezrdev <josemartinezrdev@gmail.com>
2024-05-24 10:43:32 -0500

    refactor: :art: Refractor Code

--- src/forms/form.js
+++ src/forms/form.js
@@ -1,28 +1,28 @@
 export class InsForm {
-    /**
-     * 
-     * @param {Like<Form>} formDataLike 
-     */
-    constructor({ id, name, lastName, email, country, city, cost, config = {} }) {
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.email = email;
-        this.country = country;
-        this.city = city;
-        this.cost = cost;
-        this.config = {
-            calidad: config?.calidad ?? null,
-            tipo: config?.tipo ?? null,
-            tipoAppDesk: config?.tipoAppDesk ?? null,
-            interface: config?.interface ?? null,
-            beneficio: config?.beneficio ?? null,
-            login: config?.login ?? null,
-            integracion: config?.integracion ?? null,
-            perfiles: config?.perfiles ?? null,
-            panelAdmin: config?.panelAdmin ?? null,
-            idioma: config?.idioma ?? null,
-            estado: config?.estado ?? null
-        };
-    }
+  /**
+   *
+   * @param {Like<Form>} formDataLike
+   */
+  constructor({ id, name, lastName, email, country, city, cost, config = {} }) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.email = email;
+    this.country = country;
+    this.city = city;
+    this.cost = cost;
+    this.config = {
+      calidad: config?.calidad ?? null,
+      tipo: config?.tipo ?? null,
+      tipoAppDesk: config?.tipoAppDesk ?? null,
+      interface: config?.interface ?? null,
+      beneficio: config?.beneficio ?? null,
+      login: config?.login ?? null,
+      integracion: config?.integracion ?? null,
+      perfiles: config?.perfiles ?? null,
+      panelAdmin: config?.panelAdmin ?? null,
+      idioma: config?.idioma ?? null,
+      estado: config?.estado ?? null,
+    };
+  }
 }
--- src/web_components/five_component.js
+++ src/web_components/five_component.js
@@ -1,4 +1,4 @@
-import { LitElement, css, html } from "lit";
+import { LitElement, html } from "lit";
 import "./six_component.js";
 
 class FiveComponent extends LitElement {
